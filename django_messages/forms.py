a7178f80fb4c46beb4fad50f3ea6209d0b966940
Marcin Nowak <marcin.j.nowak@gmail.com>
2011-06-04 23:21:00 +0800

    model and forms refactorings

--- django_messages/forms.py
+++ django_messages/forms.py
@@ -12,43 +12,106 @@ else:
 
 from django_messages.models import Message
 from django_messages.fields import CommaSeparatedUserField
+from django_messages.utils import format_quote
 
-class ComposeForm(forms.Form):
+
+class MessageForm(forms.ModelForm):
     """
-    A simple default form for private messages.
+    base message form
     """
-    recipient = CommaSeparatedUserField(label=_(u"Recipient"))
+    recipients = CommaSeparatedUserField(label=_(u"Recipient"))
     subject = forms.CharField(label=_(u"Subject"))
     body = forms.CharField(label=_(u"Body"),
         widget=forms.Textarea(attrs={'rows': '12', 'cols':'55'}))
-    
-    def __init__(self, *args, **kwargs):
-        recipient_filter = kwargs.pop('recipient_filter', None)
-        super(ComposeForm, self).__init__(*args, **kwargs)
+
+    class Meta:
+        model = Message
+        fields = ('recipients', 'subject', 'body',)
+
+    def __init__(self, sender, *args, **kw):
+        recipient_filter = kw.pop('recipient_filter', None)
+        self.sender = sender
+        super(MessageForm, self).__init__(*args, **kw)
         if recipient_filter is not None:
             self.fields['recipient']._recipient_filter = recipient_filter
-    
-    def save(self, sender, parent_msg=None):
+
+    def create_recipient_message(self, recipient, message):
+        return Message(
+            owner = recipient,
+            sender = self.sender,
+            to = recipient.username,
+            recipient = recipient,
+            subject = message.subject,
+            body = message.body,
+        )
+
+    def save(self, commit=True):
         recipients = self.cleaned_data['recipient']
-        subject = self.cleaned_data['subject']
-        body = self.cleaned_data['body']
+        instance = super(ComposeForm, self).save(commit=False)
+
         message_list = []
+
+        # clone messages in recipients inboxes
         for r in recipients:
-            msg = Message(
-                sender = sender,
-                recipient = r,
-                subject = subject,
-                body = body,
-            )
-            if parent_msg is not None:
-                msg.parent_msg = parent_msg
-                parent_msg.replied_at = datetime.datetime.now()
-                parent_msg.save()
-            msg.save()
+            msg = self.create_recipient_message(r, instance)
             message_list.append(msg)
-            if notification:
-                if parent_msg is not None:
-                    notification.send([r], "messages_reply_received", {'message': msg,})
-                else:
-                    notification.send([r], "messages_received", {'message': msg,})
-        return message_list
+
+        instance.to = ','.join([r.username for r in recipients])
+
+        if commit:
+            instance.save()
+            for msg in message_list:
+                msg.save()
+         
+        return instance, message_list
+
+
+class ComposeForm(MessageForm):
+    """
+    A simple default form for private messages.
+    """
+
+    class Meta:
+        model = Message
+        fields = ('recipients', 'subject', 'body',)
+    
+
+class ReplyForm(MessageForm):
+    """
+    reply to form
+    """
+    class Meta:
+        model = Message
+        fields = ('recipients', 'subject', 'body',)
+
+    def __init__(self, sender, message, *args, **kw):
+        self.parent_message = message
+        super(ReplyForm, self).__init__(self, sender, *args, **kw)
+    
+    def quote_message(self):
+        return format_quote(self.sender, self.parent_message.body)
+
+    def create_recipient_message(self, recipient, message):
+        msg = super(ReplyForm, self).create_recipient_message(recipient, message)
+        msg.replied_at = datetime.datetime.now()
+        # msg.parent_msg = ???  find parent in recipient inbox, trash or set to null
+        return msg
+
+    def save(self, commit=True):
+        instance, message_list = super(ReplyForm, self).save(commit=False)
+        instance.replied_at = datetime.datetime.now()
+        instance.parent_msg = self.parent_message
+        if commit:
+            instance.save()
+            for msg in message_list:
+                msg.save()
+        return instance, message_list
+
+
+"""
+if notification:
+    if parent_msg is not None:
+        notification.send([r], "messages_reply_received", {'message': msg,})
+    else:
+        notification.send([r], "messages_received", {'message': msg,})
+"""
--- django_messages/models.py
+++ django_messages/models.py
@@ -1,99 +1,131 @@
 import datetime
 from django.db import models
 from django.conf import settings
-from django.db.models import signals
+from django.db.models import signals, Q
 from django.db.models.query import QuerySet
 from django.contrib.auth.models import User
 from django.utils.translation import ugettext_lazy as _
 
-class MessageManager(models.Manager):
 
-    def inbox_for(self, user):
+class MessageQueryset(QuerySet):
+    def unread(self):
+        return self.filter(unread=True)
+
+
+class BaseMessageManager(models.Manager):
+    def get_query_set(self):
+        return MessageQueryset(self.model).filter(deleted=False)
+    
+    def trash(self, messages):
+        """
+        move messages to trash
+        """
+        messages.update(deleted=True, deleted_at=datetime.datetime.now())
+
+    def send(self, messages):
+        """
+        send messages
+        """
+        pass
+
+
+class Inbox(models.Manager):
+    def for_user(self, user):
         """
         Returns all messages that were received by the given user and are not
         marked as deleted.
         """
-        return self.filter(
-            recipient=user,
-            recipient_deleted_at__isnull=True,
-        )
+        return self.get_query_set().filter(recipient=user)
 
-    def outbox_for(self, user):
+
+class Outbox(models.Manager):
+    def for_user(self, user):
         """
         Returns all messages that were sent by the given user and are not
         marked as deleted.
         """
-        return self.filter(
-            sender=user,
-            sender_deleted_at__isnull=True,
-        )
+        return self.get_query_set().filter(sender=user)
 
-    def trash_for(self, user):
+
+class Trash(BaseMessageManager):
+    """
+    Trash manager
+    """
+
+    def get_query_set(self):
+        return super(Trash, self).get_query_set().filter(deleted=True)
+
+    def for_user(self, user):
         """
         Returns all messages that were either received or sent by the given
         user and are marked as deleted.
         """
-        return self.filter(
-            recipient=user,
-            recipient_deleted_at__isnull=False,
-        ) | self.filter(
-            sender=user,
-            sender_deleted_at__isnull=False,
-        )
+        return self.get_query_set().filter(Q(recipient=user)|Q(sender=user))
 
 
 class Message(models.Model):
     """
     A private message from user to user
     """
+    owner = models.ForeignKey(User, related_name='messages')
+    to = models.CharField(max_length=255)
     subject = models.CharField(_("Subject"), max_length=120)
     body = models.TextField(_("Body"))
     sender = models.ForeignKey(User, related_name='sent_messages', verbose_name=_("Sender"))
     recipient = models.ForeignKey(User, related_name='received_messages', null=True, blank=True, verbose_name=_("Recipient"))
     parent_msg = models.ForeignKey('self', related_name='next_messages', null=True, blank=True, verbose_name=_("Parent message"))
     sent_at = models.DateTimeField(_("sent at"), null=True, blank=True)
+    unread = models.BooleanField(default=True, db_index=True)
     read_at = models.DateTimeField(_("read at"), null=True, blank=True)
     replied_at = models.DateTimeField(_("replied at"), null=True, blank=True)
-    sender_deleted_at = models.DateTimeField(_("Sender deleted at"), null=True, blank=True)
-    recipient_deleted_at = models.DateTimeField(_("Recipient deleted at"), null=True, blank=True)
+    deleted = models.BooleanField(default=False, db_index=True)
+    deleted_at = models.DateTimeField(_("Sender deleted at"), null=True, blank=True)
+
+    objects = BaseMessageManager()
+    inbox = Inbox()
+    outbox = Outbox()
+    trash = Trash()
     
-    objects = MessageManager()
-    
-    def new(self):
+    def is_unread(self):
         """returns whether the recipient has read the message or not"""
-        if self.read_at is not None:
-            return False
-        return True
+        return bool(self.read_at is None)
+
+    def read(self):
+        self.unread = False
+        self.read_at = datetime.datetime.now()
+
+    def unread(self):
+        self.unread = True
+        self.read_at = None
+
+    def trash(self):
+        self.deleted = True
+        self.deleted_at = datetime.datetime.now()
         
     def replied(self):
         """returns whether the recipient has written a reply to this message"""
-        if self.replied_at is not None:
-            return True
-        return False
+        return bool(self.replied_at is not None)
     
     def __unicode__(self):
         return self.subject
     
+    @models.permalink
     def get_absolute_url(self):
-        return ('messages_detail', [self.id])
-    get_absolute_url = models.permalink(get_absolute_url)
-    
-    def save(self, *args, **kwargs):
-        if not self.id:
-            self.sent_at = datetime.datetime.now()
-        super(Message, self).save(*args, **kwargs) 
+        return ('messages_detail', (self.id,))
     
     class Meta:
         ordering = ['-sent_at']
         verbose_name = _("Message")
         verbose_name_plural = _("Messages")
         
+
 def inbox_count_for(user):
     """
     returns the number of unread messages for the given user but does not
     mark them seen
     """
-    return Message.objects.filter(recipient=user, read_at__isnull=True, recipient_deleted_at__isnull=True).count()
+    return Message.inbox.for_user(user).unread().count()
+
 
 # fallback for email notification if django-notification could not be found
 if "notification" not in settings.INSTALLED_APPS:
